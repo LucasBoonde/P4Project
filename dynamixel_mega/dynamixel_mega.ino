2de66e0d96d86d0aebb4cb82910e7e82d665925a
Kaspso <kaspso21@student.aau.dk>
2023-05-16 13:47:05 +0200

    Update dynamixel_mega.ino
    Tilføjede dynamixel library og satte motorerne op til at køre til en position, og sende deres nuværende position tilbage gennem Serial1

--- dynamixel_mega/dynamixel_mega.ino
+++ dynamixel_mega/dynamixel_mega.ino
@@ -1,8 +1,68 @@
+/*******************************************************************************
+* Copyright 2016 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include <DynamixelShield.h>
+
+const uint8_t DXL_ID = 1;
+const uint8_t DXL_ID2 = 2;
+
+const float DXL_PROTOCOL_VERSION = 2.0;
+
+DynamixelShield dxl;
+
+//This namespace is required to use Control table item names
+using namespace ControlTableItem;
+
+
+
 void setup() {
   //Åbne serial forbindelse
   Serial.begin(115200);
   Serial1.begin(115200);
   Serial.setTimeout(1);
+
+
+  // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
+  dxl.begin(57600);
+  // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+  // Get DYNAMIXEL information
+  dxl.ping(DXL_ID);
+
+  // Turn off torque when configuring items in EEPROM area - Jeg ved faktisk ikke om man skal sætte den i position mode for at aflæse positionen, men det tror jeg
+  dxl.torqueOff(DXL_ID);
+  dxl.setOperatingMode(DXL_ID, OP_POSITION);
+  dxl.torqueOn(DXL_ID);
+
+  dxl.ping(DXL_ID2);
+
+  // Turn off torque when configuring items in EEPROM area
+  dxl.torqueOff(DXL_ID2);
+  dxl.setOperatingMode(DXL_ID2, OP_POSITION);
+  dxl.torqueOn(DXL_ID2);
+
+
+
+  // Set velocity and acceleration (ONLY RELEVANT FOR TESTING IN POSITION CONTROL MODE, OUTCOMMENT FOR CURRENT MODE)
+  dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID, 50);
+  dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 1000);
+  
+  dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID2, 50);
+  dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID2, 1000);
+
 }
 
 void loop() 
@@ -39,6 +99,22 @@ void loop()
   }
   Serial.print(String(Tau[0]));
   String output = "#" +String(Tau[0])+"og"+String(Tau[1])+"#Modtaget";
+
+
+  //Kør robotten til de givne Tau* værdier (Vi prøver først lige med positions værdier i grader) 
+  dxl.setGoalPosition(DXL_ID, Tau[0], UNIT_DEGREE);
+  dxl.setGoalPosition(DXL_ID2, Tau[1], UNIT_DEGREE);
+  delay(1000);
+
+
+  //Print nuværende position til Serial1 linjen
+  Serial1.print("Present Position 1(degree) : ");
+  Serial1.println(dxl.getPresentPosition(DXL_ID, UNIT_DEGREE));
+
+  Serial1.print("Present Position 2(degree) : ");
+  Serial1.println(dxl.getPresentPosition(DXL_ID2, UNIT_DEGREE));
+
+
   Serial1.print(message);
   Serial1.println(output);
 }
